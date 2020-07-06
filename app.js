8c2a9532d3fc80bdd1223d03d10ee504e756a390
paulo.fernandes <phenrique.fernandes@yahoo.com>
2020-07-05 21:56:39 -0300

    Encapsula variaveis ambientes com dotenv

--- app.js
+++ app.js
@@ -4,34 +4,36 @@ import mongoose from 'mongoose';
 
 import { studentRouter } from './routes/studentRouter.js';
 
+require('dotenv').config();
+
 /* Conexão com o MongoDB */
 // url para conexao base local
-const url = 'mongodb://localhost/grades';
+// const url = 'mongodb://localhost/grades';
 
 // url para conexao base cloud (Mongo DB Atlas)
-// const url =
-//   'mongodb+srv://admin:mgDBgt@cluster0-a4kgi.mongodb.net/grades?retryWrites=true&w=majority';
-mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(console.log('Conectado ao MongoDB local'))
-  .catch((err) => console.log('Erro ao conectar no MongoDB local ' + err));
+const url = `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@cluster0-a4kgi.mongodb.net/grades?retryWrites=true&w=majority`;
 
-// async () => {
-//   try {
-//     await mongoose.connect(url, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//   } catch (error) {
-//     console.log('Erro ao conectar no MongoDB ' + error);
-//   }
-// };
+// mongoose
+//   .connect(url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(console.log('Conectado ao MongoDB local'))
+//   .catch((err) => console.log('Erro ao conectar no MongoDB local ' + err));
+
+(async () => {
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.log('Erro ao conectar no MongoDB ' + error);
+  }
+})();
 
 const app = express();
 app.use(express.json());
 app.use(studentRouter);
 
-app.listen(3000, () => console.log('API iniciada'));
+app.listen(process.env.PORT, () => console.log('API iniciada'));
